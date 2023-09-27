42133b26586f8cec6de820312d0e3243f3cfc689
Bayero-abdul <bayeroa65@gmail.com>
2023-09-27 20:52:37 +0100

    Test code

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,17 +1,18 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
     .then((results) => {
       const arr = [];
+
       for (const result of results) {
         if (result.status === 'fulfilled') {
           arr.push({ status: result.status, value: result.value });
         } else {
           arr.push({
             status: result.status,
-            value: `Error: ${fileName} cannot  be processed`,
+            value: `Error: ${filename} cannot be processed`,
           });
         }
       }
