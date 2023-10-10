ae36b7a089cbcc378162c80c0ab47968bd7e936e
Bayero-abdul <bayeroa65@gmail.com>
2023-10-10 21:52:54 +0100

    Add getters and setters

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -9,6 +9,32 @@ export default class Currency {
     else throw new TypeError('Name must be a string');
   }
 
+  // getter for code
+  get code() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._code;
+  }
+
+  // setter for code
+  set code(value) {
+    // eslint-disable-next-line no-underscore-dangle
+    if (typeof value === 'string') this._code = value;
+    else throw new TypeError('Code must be a string');
+  }
+
+  // getter for name
+  get name() {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._name;
+  }
+
+  // setter for name
+  set name(value) {
+    // eslint-disable-next-line no-underscore-dangle
+    if (typeof value === 'string') this._name = value;
+    else throw new TypeError('Name must be a string');
+  }
+
   displayFullCurrency() {
     // eslint-disable-next-line no-underscore-dangle
     return `${this._name} (${(this._code)})`;
