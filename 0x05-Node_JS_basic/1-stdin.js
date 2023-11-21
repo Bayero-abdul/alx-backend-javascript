71f8e11f8906b6a1f53f82fe737a0d828114c445
Bayero-abdul <bayeroa65@gmail.com>
2023-11-21 22:03:16 +0100

    Test code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,15 @@
-process.stdin.setEncoding('utf8');
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is ${chunk}`);
+  }
 });
 
 process.on('exit', () => {
   if (!process.stdin.isTTY) {
     process.stdout.write('This important software is now closing\n');
-    process.exit();
   }
 });
